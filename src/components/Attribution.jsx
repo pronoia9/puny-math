11e4e0b0348fe1885f8d04ca5def6ebc291086dd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 17:38:01 +0300

    edit attribution

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -3,12 +3,8 @@ import React from 'react';
 const Attribution = () => {
   return (
     <div class='attribution'>
-      Challenge by{' '}
-      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
-        Frontend Mentor
-      </a>
-      . Coded by
-      <a href='#'>Your Name Here</a>.
+      Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>Frontend Mentor</a>.
+      Coded by<a href='https://github.com/pronoia9' target='_blank' rel='noreferrer'>Jay Ansin</a>.
     </div>
   );
 };
