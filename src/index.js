8beda2b96f94d0990e848ef75c1922f2b5c257c5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 17:36:04 +0300

    add context provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ContextProvider } from './contexts/StateContext';
 import './index.scss';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
