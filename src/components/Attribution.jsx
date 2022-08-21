b965654aefa9e264d15340f3b1f4ec552dd7899b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 17:01:16 +0300

    add attribution component

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Attribution = () => {
+  return (
+    <div class='attribution'>
+      Challenge by{' '}
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a href='#'>Your Name Here</a>.
+    </div>
+  );
+};
+
+export default Attribution;
