8b8003b9bf95ae1c5aebc2de92332922f11a5036
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 21:10:27 +0300

    change del function, add states to value

--- src/contexts/StateContext.js
+++ src/contexts/StateContext.js
@@ -4,12 +4,39 @@ const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [theme, setTheme] = useState('dark');
+  const [input, setInput] = useState(0);
+  const [calculations, setCalculations] = useState('');
+  const [result, setResult] = useState('');
 
-  const themeButtonHandler = (event) => {
-    setTheme(event.target.value);
-  };
+  const themeButtonHandler = (event) => { setTheme(event.target.value); };
 
-  return <StateContext.Provider value={{ theme, setTheme, themeButtonHandler }}>{children}</StateContext.Provider>;
+  const addition = (num) => { setResult(result + num); };
+  const subtraction = (num) => { setResult(result - num); };
+  const multiplication = (num) => { setResult(result * num); };
+  const division = (num) => { setResult(result / num); };
+
+  const calculate = () => {
+    // +, -, x, /, =
+  }
+
+  const showResult = () => { setInput(result); }
+
+  const clear = () => {
+    setInput(0);
+    setCalculations('');
+    setResult('');
+  }
+
+  const del = () => { 
+    if (input.toString().length > 0) setInput(parseInt(input.toString().slice(input.length - 2)));
+   }
+
+  return (
+    <StateContext.Provider
+      value={{ theme, input, calculations, result, themeButtonHandler, }}>
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
