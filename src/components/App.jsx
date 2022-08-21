2c4b04ca44e5f31c963c262afa1468efd3fcab9e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 18:17:40 +0300

    add theme context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
-import './App.scss';
 import Header from './Header';
+import './App.scss';
+
+import { useStateContext } from '../contexts/StateContext';
 
 export default function App() {
+  const { theme } = useStateContext();
+
   return (
-    <div className='wrapper dark-colors'>
+    <div className={`wrapper ${theme}-colors`}>
       <div className='app'>
         <Header />
       </div>
