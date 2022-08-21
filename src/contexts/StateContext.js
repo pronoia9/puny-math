f5cbdd24d726e043ae5d62f73a3ad70a008bbb28
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 22:47:09 +0300

    fix calc button order

--- src/contexts/StateContext.js
+++ src/contexts/StateContext.js
@@ -60,24 +60,24 @@ export const ContextProvider = ({ children }) => {
   };
 
   const buttons = [
-    { button: '1', type: 'number', style: '', click: addInput },
-    { button: '2', type: 'number', style: '', click: addInput },
-    { button: '3', type: 'number', style: '', click: addInput },
-    { button: '4', type: 'number', style: '', click: addInput },
-    { button: '5', type: 'number', style: '', click: addInput },
-    { button: '6', type: 'number', style: '', click: addInput },
     { button: '7', type: 'number', style: '', click: addInput },
     { button: '8', type: 'number', style: '', click: addInput },
     { button: '9', type: 'number', style: '', click: addInput },
-    { button: '0', type: 'number', style: '', click: addInput },
-    { button: '.', type: 'operator', style: '', click: addDecimal },
+    { button: 'DEL', type: 'operator', style: '', click: del },
+    { button: '4', type: 'number', style: '', click: addInput },
+    { button: '5', type: 'number', style: '', click: addInput },
+    { button: '6', type: 'number', style: '', click: addInput },
     { button: '+', type: 'operator', style: '', click: calculate },
+    { button: '1', type: 'number', style: '', click: addInput },
+    { button: '2', type: 'number', style: '', click: addInput },
+    { button: '3', type: 'number', style: '', click: addInput },
     { button: '-', type: 'operator', style: '', click: calculate },
-    { button: 'x', type: 'operator', style: '', click: calculate },
+    { button: '.', type: 'operator', style: '', click: addDecimal },
+    { button: '0', type: 'number', style: '', click: addInput },
     { button: '/', type: 'operator', style: '', click: calculate },
-    { button: '=', type: 'operator', style: '', click: calculate },
-    { button: 'DEL', type: 'operator', style: ' double-size', click: del },
+    { button: 'x', type: 'operator', style: '', click: calculate },
     { button: 'RESET', type: 'operator', style: ' double-size', click: reset },
+    { button: '=', type: 'operator', style: ' double-size', click: calculate },
   ];
 
   return (
