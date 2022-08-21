8117665bef98044f5b6ed9cf6eda9a6362fd7673
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 21:16:44 +0300

    add keys/buttons arr

--- src/contexts/StateContext.js
+++ src/contexts/StateContext.js
@@ -2,6 +2,27 @@ import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
+const keys = [
+  { key: '1', type: 'number', style: '' },
+  { key: '2', type: 'number', style: '' },
+  { key: '3', type: 'number', style: '' },
+  { key: '4', type: 'number', style: '' },
+  { key: '5', type: 'number', style: '' },
+  { key: '6', type: 'number', style: '' },
+  { key: '7', type: 'number', style: '' },
+  { key: '8', type: 'number', style: '' },
+  { key: '9', type: 'number', style: '' },
+  { key: '0', type: 'number', style: '' },
+  { key: '.', type: 'operator', style: '' },
+  { key: '+', type: 'operator', style: '' },
+  { key: '-', type: 'operator', style: '' },
+  { key: 'x', type: 'operator', style: '' },
+  { key: '/', type: 'operator', style: '' },
+  { key: '=', type: 'operator', style: '' },
+  { key: 'DEL', type: 'operator', style: 'double-size' },
+  { key: 'RESET', type: 'operator', style: 'double-size' },
+];
+
 export const ContextProvider = ({ children }) => {
   const [theme, setTheme] = useState('dark');
   const [input, setInput] = useState(0);
@@ -33,7 +54,7 @@ export const ContextProvider = ({ children }) => {
 
   return (
     <StateContext.Provider
-      value={{ theme, input, calculations, result, themeButtonHandler, }}>
+      value={{ theme, input, calculations, result, keys, themeButtonHandler, }}>
       {children}
     </StateContext.Provider>
   );
