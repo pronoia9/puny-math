8acacb2e1f0078aab2e6480bf1e2b5e6fea5764e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-21 22:45:37 +0300

    add check to show result and new calculation

--- src/contexts/StateContext.js
+++ src/contexts/StateContext.js
@@ -7,40 +7,56 @@ export const ContextProvider = ({ children }) => {
   const [input, setInput] = useState('0');
   const [calculations, setCalculations] = useState('');
   const [result, setResult] = useState('');
+  const [showResult, setShowResult] = useState(false);
 
   const themeButtonHandler = (event) => {
     setTheme(event.target.value);
   };
 
   const addInput = (num) => {
-    setInput(`${parseFloat(input + num)}`);
-  }
-
-  const addDecimal = () => { 
-    setInput(`${parseFloat(input)}${!input.includes('.') ? '.' : ''}`);
+    if (showResult) {
+      setShowResult(false);
+      setInput(`${parseFloat(num)}`);
+    } else setInput(`${parseFloat(input + num)}`);
   };
 
-  const addition = () => { setResult(`${parseInt(result + input)}`); };
-  const subtraction = () => { setResult(`${parseInt(result - input)}`); };
-  const multiplication = () => { setResult(`${parseInt(result * input)}`); };
-  const division = () => { setResult(`${parseInt(result / input)}`); };
-
-  const calculate = () => {
-    // +, -, x, /, =
+  const addDecimal = () => {
+    if (!showResult) setInput(`${parseFloat(input)}${!input.includes('.') ? '.' : ''}`);
   };
 
-  const showResult = () => {
-    setInput(result);
+  const addition = () => `${parseFloat(result) + parseFloat(input)}`;
+  const subtraction = () => `${parseFloat(result) - parseFloat(input)}`;
+  const multiplication = () => `${parseFloat(result) * parseFloat(input)}`;
+  const division = () => `${parseFloat(result) / parseFloat(input)}`;
+
+  const calculate = (btn) => {
+    if (input.length) {
+      if (btn === '=') {
+        setInput(result || input);
+        setShowResult(true);
+      } else {
+        setCalculations(`${calculations}${input} ${btn} `);
+        if (result.length) {
+          // set/calculate result
+          if (btn === '+') setResult(addition());
+          else if (btn === '-') setResult(subtraction());
+          else if (btn === '*') setResult(multiplication());
+          else if (btn === '/') setResult(division());
+        } else setResult(input);
+        // clear input
+        setInput('');
+      }
+    }
   };
 
   const reset = () => {
-    setInput(0);
+    setInput('0');
     setCalculations('');
     setResult('');
   };
 
   const del = () => {
-    if (input.length > 0) setInput(input.slice(input.length - 2));
+    if (input.length > 0) setInput(input.slice(0, input.length - 1));
   };
 
   const buttons = [
@@ -55,17 +71,17 @@ export const ContextProvider = ({ children }) => {
     { button: '9', type: 'number', style: '', click: addInput },
     { button: '0', type: 'number', style: '', click: addInput },
     { button: '.', type: 'operator', style: '', click: addDecimal },
-    { button: '+', type: 'operator', style: '', click: addition },
-    { button: '-', type: 'operator', style: '', click: subtraction },
-    { button: 'x', type: 'operator', style: '', click: multiplication },
-    { button: '/', type: 'operator', style: '', click: division },
+    { button: '+', type: 'operator', style: '', click: calculate },
+    { button: '-', type: 'operator', style: '', click: calculate },
+    { button: 'x', type: 'operator', style: '', click: calculate },
+    { button: '/', type: 'operator', style: '', click: calculate },
     { button: '=', type: 'operator', style: '', click: calculate },
     { button: 'DEL', type: 'operator', style: ' double-size', click: del },
     { button: 'RESET', type: 'operator', style: ' double-size', click: reset },
   ];
 
   return (
-    <StateContext.Provider value={{ theme, input, calculations, result, buttons, themeButtonHandler }}>
+    <StateContext.Provider value={{ theme, input, calculations, result, buttons, showResult, themeButtonHandler }}>
       {children}
     </StateContext.Provider>
   );
