e40879ce5dd17d043c56d4f177cb2397d1459a00
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-22 20:37:42 +0300

    add even listener for keydowns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,16 @@ import './App.scss';
 import { useStateContext } from '../contexts/StateContext';
 
 export default function App() {
-  const { theme } = useStateContext();
+  const { theme, addInput, calculate, del, getResult } = useStateContext();
+
+  window.addEventListener('keydown', (event) => {
+    const operators = ['+', '-', 'x', '*', '/'], key = event.key;
+    console.log(key);
+    if (key >= 0 && key <= 9) addInput(key);
+    else if (operators.includes(key)) calculate(key);
+    else if (key === 'Backspace' || key === 'Delete') del();
+    else if (key === '=' || key === 'Enter') getResult();
+  })
 
   return (
     <div className={`wrapper ${theme}-colors`}>
