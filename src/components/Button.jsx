cc2743401a678d6df6792f563e6207e9f220fe9c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-10 16:56:48 +0300

    add button stylesheet

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,5 @@
+import './Button.scss';
+
 export default function Button({ button, type, style, click }) {
   return <button className={`btn${style}`} onClick={() => click(button)}>{button}</button>;
 }
