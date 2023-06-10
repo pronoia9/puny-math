2386bfc12f1dec38fe030d0bbc66984ba772cafb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 18:05:55 +0300

    vite init

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useEffect } from 'react';
-import './App.scss';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { useStateContext } from './contexts/StateContext';
-import Header from './components/Header';
-import Results from './components/Results';
-import Calculator from './components/Calculator';
-
-export default function App() {
-  const { theme, addInput, calculate, del, getResult } = useStateContext();
-
-  // change theme
-  useEffect(() => {
-    document.querySelector('body').className = `${theme}-theme`;
-  }, [theme])
-
-  window.addEventListener('keydown', (event) => {
-    const operators = ['+', '-', 'x', '*', '/'], key = event.key;
-    console.log(key);
-    if (key >= 0 && key <= 9) addInput(key);
-    else if (operators.includes(key)) calculate(key);
-    else if (key === 'Backspace' || key === 'Delete') del();
-    else if (key === '=' || key === 'Enter') getResult();
-  })
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className='wrapper'>
-      <div className='app'>
-        <Header />
-        <main>
-          <Results />
-          <Calculator />
-        </main>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
--- src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-import './Button.scss';
-
-export default function Button({ button, type, style, click }) {
-  return <button className={`btn${style}`} onClick={() => click(button)}>{button}</button>;
-}
