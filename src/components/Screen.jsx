2a5e2b5a08c44b86344cb2b206b7e651722e2ada
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 22:34:42 +0300

    finish screen

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,3 +1,38 @@
+import { styled } from 'styled-components';
+import { useStore } from '../store/useStore';
+
 export default function Screen() {
-  return <div>Screen</div>;
+  const input = useStore((state) => state.input);
+  
+  return (
+    <Container className='display border-radius'>
+      <Calculations>
+        {'calculations added here maybe idk might delete'}
+      </Calculations>
+      <div>
+        {input || '0'}
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  margin-block: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 2.5rem;
+  font-weight: 700;
+  color: var(--color-text-header);
+  text-align: right;
+  background: var(--color-bg-screen);
+  border-radius: 10px;
+
+  @media screen and (min-width: 42rem) {
+    font-size: 3.5rem;
+  }
+`;
+
+const Calculations = styled.div`
+  font-size: 1rem;
+  font-weight: 500;
+  min-height: 1.5rem;
+`;
