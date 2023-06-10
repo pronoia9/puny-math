4fe42847a9e3abca794f1926e3a0d8383da402e8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 19:47:22 +0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, styled } from 'styled-components';
 
+import { Header } from './components';
 import { useStore } from './store/Store';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { getTheme } from './utils/utils';
 
-const App = () => {
+export default function App() {
   // STORE
   const theme = useStore((state) => state.theme);
 
   return (
     <ThemeProvider theme={getTheme(theme)}>
       <GlobalStyles />
+
+      <Container>
+        <Wrapper>
+          <Header />
+        </Wrapper>
+      </Container>
     </ThemeProvider>
   );
-};
+}
 
-export default App;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: var(--color-bg-main);
+`;
+
+const Wrapper = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 1.7rem;
+  max-width: 37.25rem;
+
+  @media screen and (min-width: 42rem) {
+    padding-block: 6rem;
+  }
+`;
