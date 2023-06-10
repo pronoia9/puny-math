aafce65128b29905a911657348124cd3a0ce3352
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 19:31:01 +0300

    add init theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <></>;
-}
+import { ThemeProvider } from 'styled-components';
+
+import { useStore } from './store/Store';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { getTheme } from './utils/utils';
+
+const App = () => {
+  // STORE
+  const theme = useStore((state) => state.theme);
+
+  return (
+    <ThemeProvider theme={getTheme(theme)}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/styles/Themes.js
@@ -0,0 +1,71 @@
+export const darkTheme = {
+  // Backgrounds
+  bgMain: 'hsl(222, 26%, 31%)',
+  bgToggle: 'hsl(223, 31%, 20%)',
+  bgKeypad: 'hsl(223, 31%, 20%)',
+  bgScreen: 'hsl(224, 36%, 15%)',
+  // Keys
+  keysBg1: 'hsl(30, 25%, 89%)',
+  keysShadow1: 'hsl(28, 16%, 65%)',
+  keysHover1: 'hsl(0, 0, 100%)',
+  keysBg2: 'hsl(225, 21%, 49%)',
+  keysShadow2: 'hsl(224, 28%, 35%)',
+  keysHover2: '',
+  keysBg3: 'hsl(6, 63%, 50%)',
+  keysShadow3: 'hsl(6, 70%, 34%)',
+  keysHover3: '',
+  keysToggle: 'hsl(6, 63%, 50%)',
+  // Text
+  textHeader: 'hsl(0, 0, 100%)',
+  textKeys: 'hsl(221, 14%, 31%)',
+  textEdit: 'hsl(0, 0, 100%)',
+  textSubmit: 'hsl(0, 0, 100%)',
+};
+
+export const lightTheme = {
+  // Backgrounds
+  bgMain: 'hsl(0, 0%, 90%)',
+  bgToggle: 'hsl(0, 5%, 81%)',
+  bgKeypad: 'hsl(0, 5%, 81%)',
+  bgScreen: 'hsl(0, 0%, 93%)',
+  // Keys
+  keysBg1: 'hsl(45, 7%, 89%)',
+  keysShadow1: 'hsl(35, 11%, 61%)',
+  keysHover1: 'hsl(0, 0, 100%)',
+  keysBg2: 'hsl(185, 42%, 37%)',
+  keysShadow2: 'hsl(185, 58%, 25%)',
+  keysHover2: '',
+  keysBg3: 'hsl(25, 98%, 40%)',
+  keysShadow3: 'hsl(25, 99%, 27%)',
+  keysHover3: '',
+  keysToggle: 'hsl(25, 98%, 40%)',
+  // Text
+  textHeader: 'hsl(60, 10%, 19%)',
+  textKeys: 'hsl(60, 10%, 19%)',
+  textEdit: 'hsl(0, 0, 100%)',
+  textSubmit: 'hsl(0, 0, 100%)',
+};
+
+export const retroTheme = {
+  // Backgrounds
+  bgMain: 'hsl(268, 75%, 9%)',
+  bgToggle: 'hsl(268, 71%, 12%)',
+  bgKeypad: 'hsl(268, 71%, 12%)',
+  bgScreen: 'hsl(268, 71%, 12%)',
+  // Keys
+  keysBg1: 'hsl(268, 47%, 21%)',
+  keysShadow1: 'hsl(290, 70%, 36%)',
+  keysHover1: 'hsl(176, 100%, 44%)',
+  keysBg2: 'hsl(281, 89%, 26%)',
+  keysShadow2: 'hsl(285, 91%, 52%)',
+  keysHover2: 'hsl(176, 100%, 44%)',
+  keysBg3: 'hsl(176, 100%, 44%)',
+  keysShadow3: 'hsl(177, 92%, 70%)',
+  keysHover3: 'hsl(176, 100%, 44%)',
+  keysToggle: 'hsl(176, 100%, 44%)',
+  // Text
+  textHeader: 'hsl(52, 100%, 62%)',
+  textKeys: 'hsl(52, 100%, 62%)',
+  textEdit: 'hsl(0, 0, 100%)',
+  textSubmit: 'hsl(198, 20%, 13%)',
+};
