0a9dadb32868f82a6658bd7a0ad47160968d1a77
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-10 22:34:38 +0300

    add input to store

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -3,4 +3,7 @@ import { create } from 'zustand';
 export const useStore = create((set) => ({
   theme: 'dark',
   setTheme: (newTheme) => set({ theme: newTheme }),
+  
+  input: '',
+  setInput: (newInput) => set({ input: newInput }),
 }));
