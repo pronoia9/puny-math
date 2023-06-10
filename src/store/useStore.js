086488b65f1018274f6666bcb93bbdc7c68e373b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-11 00:43:01 +0300

    add some funcs

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,9 +1,24 @@
 import { create } from 'zustand';
+import { deleteLast } from '../utils/utils';
 
 export const useStore = create((set) => ({
   theme: 'dark',
   setTheme: (newTheme) => set({ theme: newTheme }),
-  
+
   input: '',
   setInput: (newInput) => set({ input: newInput }),
+  addInput: (newInput) =>
+    set((state) => {
+      return { input: `${state.input}${newInput}` };
+    }),
+  removeInput: () =>
+    set((state) => {
+      return { input: `${deleteLast(state.input)}` };
+    }),
+  resetInput: () => set({ input: '' }),
+
+  calculations: '',
+  resetCalculations: () => set({ calculations: '' }),
+
+  reset: () => set({ input: '', calculations: '' }),
 }));
