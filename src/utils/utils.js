2f832dc9e163096fd05f27678adffb7fe77d5519
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-11 00:21:04 +0300

    add some basic functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 import { darkTheme, lightTheme, retroTheme } from '../styles/Themes';
 
-export function getTheme(theme) {
+export const getTheme = (theme) => {
   switch (theme) {
     case 'dark':
       return darkTheme;
@@ -10,3 +10,21 @@ export function getTheme(theme) {
       return retroTheme;
   }
 }
+
+export const isNumber = (input) => !isNaN(parseFloat(input));
+
+export const add = (a, b) => parseFloat(a) + parseFloat(b);
+
+export const subtract = (a, b) => parseFloat(a) - parseFloat(b);
+
+export const multiply = (a, b) => parseFloat(a) * parseFloat(b);
+
+export const divide = (a, b) => parseFloat(a) / parseFloat(b);
+
+export const deleteLast = (input) => input.length !== 0 ? input.slice(0, -1) : input;
+
+export const keyClick = ({ button, type, click }) => {
+  if (type === 'number') {
+  } else if (type === 'operator') {
+  }
+};
