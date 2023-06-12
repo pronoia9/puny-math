cfa589a9a48b96cd34e4642a368491d9228d663a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-12 22:29:57 +0300

    add react tooltip to input

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,20 +1,24 @@
 import { styled } from 'styled-components';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 import { useStore } from '../store/useStore';
 import { copyToClipboard } from '../utils/utils';
+import { useRef } from 'react';
 
 export default function Screen() {
   const input = useStore((state) => state.input),
     calculations = useStore((state) => state.calculations);
+  const calculationsRef = useRef(),
+    inputRef = useRef();
   
   return (
     <Container>
-      <Calculations onClick={() => copyToClipboard(calculations)}>
-        {calculations}
-      </Calculations>
-      <Input onClick={() => copyToClipboard(input)}>
-        <span>{input}</span>
-      </Input>
+      <Calculations className='calculations' ref={calculationsRef} onClick={() => copyToClipboard(calculations)}>{calculations}</Calculations>
+      {/* {calculations && <Tooltip id='calculations-tooltip' anchorSelect='.calculations'>{calculations}</Tooltip>} */}
+
+      <Input className='input' ref={inputRef} onClick={() => copyToClipboard(input)}>{input}</Input>
+      {input && input !== '0' && input.length > 16 && <Tooltip id='input-tooltip' anchorSelect='.input'>{input || 0}</Tooltip>}
     </Container>
   );
 }
@@ -28,6 +32,16 @@ const Container = styled.div`
   border-radius: 10px;
   transition: all 0.5s ease-in-out;
   height: 100%;
+
+  .react-tooltip {
+    border-radius: 10px;
+    color: var(--color-text-header);
+    background: var(--color-bg-screen);
+
+    &#input-tooltip {
+      font-size: 1.5rem;
+    }
+  }
 
   @media screen and (min-width: 42rem) {
     font-size: 3.5rem;
@@ -44,7 +58,6 @@ const Calculations = styled.div`
 
   &:hover {
     cursor: pointer;
-    overflow: visible;
   }
 `;
 
@@ -56,7 +69,6 @@ const Input = styled.div`
 
   &:hover {
     cursor: pointer;
-    overflow: visible;
   }
 
   @media screen and (min-width: 42rem) {
